6a14e46e1ce5fd95a102351b9fe17c7eb9633e70
biaov <biaov@qq.com>
2025-03-04 13:30:41 +0800

    更新依赖并优化代码

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,4 +1,3 @@
-import 'tailwindcss/tailwind.css'
 import '@/styles/reset.less'
 
 export const install = () => {}
--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,14 @@ import { resolve } from 'path'
 import eslint from 'vite-plugin-eslint'
 import autoImport from 'unplugin-auto-import/vite'
 import components from 'unplugin-vue-components/vite'
-import tailwindcss from 'tailwindcss'
-
-const spacing: Record<string, string> = {}
-
-Array.from({ length: 1000 }, (_, i) => {
-  spacing[i] = `${i}rpx`
-})
+import tailwindcss from '@tailwindcss/vite'
 
 const env = loadEnv('development', './')
 
 export default defineConfig({
   base: './',
   plugins: [
+    tailwindcss(),
     eslint({
       lintOnStart: true,
       exclude: ['node_modules', 'dist', 'fonts']
@@ -65,23 +60,6 @@ export default defineConfig({
       less: {
         additionalData: `@import '@/styles/vars.less';`
       }
-    },
-    postcss: {
-      plugins: [
-        tailwindcss({
-          content: ['./src/**/*.vue'],
-          theme: {
-            spacing,
-            extend: {
-              fontSize: ({ theme }) => theme('spacing'),
-              borderRadius: ({ theme }) => theme('spacing')
-            }
-          },
-          corePlugins: {
-            preflight: false
-          }
-        })
-      ]
     }
   }
 })
